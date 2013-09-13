1e2900b52d4c77b618233c79fa8f08c1875087c9
Sasha Khamkov <sanusart@gmail.com>
2013-09-14 00:33:03 +0300

    Add git ignore, Add 'joinBy','truncate','removeFirst','removeLast' filters

--- string-filters.js
+++ string-filters.js
@@ -1,1 +1,20 @@
+'use strict';
 
+angular.module('App.filters', [])
+    .filter('joinBy', function () {
+        return function (input,delimiter) {
+            return (input || []).join(delimiter || ',');
+        };
+}).filter('removeFirst', function () {
+        return function (input) {
+            return input.substring(1);
+        };
+}).filter('removeLast', function () {
+        return function (input) {
+            return input.slice(0,input.length-1);
+        };
+}).filter('truncate', function () {
+        return function (input,limit,endChar) {
+            return input.substr(0,limit || 80) + ((limit >= input.length) ? '' : endChar || '...');
+        };
+}); 
