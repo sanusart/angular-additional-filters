a929fa3915c7b44524517f0757aecea742821b57
Maayan Glikser <maayan@glikm.com>
2014-07-21 17:08:56 +0300

    Add capitalize and kCurrency Filters
    Capitalize - Capitalize the first character in a string
    kCurrency - Same syntax as angular currency filter only returns a shorter rounded version of the currency for display purposes. e.g: 5K 5.3K 5M 5.5M 55M

--- filters.js
+++ filters.js
@@ -21,4 +21,90 @@ angular.module('App.filters', [])
         return function (input) {
             return input.replace(/(<([^>]+)>)/ig,'');
         };
+    }).filter('capitalize', function () {
+        return function (input) {
+            if (input) {
+                return input.toLowerCase().substring(0, 1).toUpperCase() + input.substring(1);
+            }
+            return input;
+        }
+    }).filter('kCurrency', function () {
+        
+        /**
+         * Custom Currency Filter
+         * This filter given the following inputs will render the following outputs:
+         * Input: 50,000 Output: 50K
+         * Input 50321 Output: 50.3K
+         * Input: 5,000,000 Output: 5M
+         */
+
+        var formatNumber = function(number) {
+            if (!number) return '';
+
+            var parseNumber = function(number, endIndex, symbol) {
+                var major = number.slice(0, endIndex),
+                    minor = number.slice(endIndex, endIndex + 1);
+
+                return minor == 0 ? major + symbol : major + '.' + minor + symbol;
+            };
+
+            // parse number of digits in a number and format accordingly
+            var digitsRegex = /([\d]+)/g;
+            var matches = number.match(digitsRegex);
+            if (matches.length > 0) {
+                var numberOfDigits = matches[0].length;
+
+                /*
+                    We can identify that the number is in the thousands
+                    range if the number is between 4 and 6 digits.
+                 */
+                if (numberOfDigits === 4) {
+                    return parseNumber(number, 1, 'K');
+                } else if (numberOfDigits === 5) {
+                    return parseNumber(number, 2, 'K');
+                } else if (numberOfDigits === 6) {
+                    return parseNumber(number, 3, 'K');
+                } else if (numberOfDigits === 7) {
+                    return parseNumber(number, 1, 'M');
+                } else if (numberOfDigits === 8) {
+                    return parseNumber(number, 2, 'M');
+                }
+            }
+
+            // number does not fit range return original number
+            return number;
+
+        };
+
+
+        var formats = $locale.NUMBER_FORMATS;
+        return function (amount, currencySymbol) {
+            if (amount) {
+                var parts = [];
+
+                if (angular.isUndefined(currencySymbol)) {
+                    currencySymbol = formats.CURRENCY_SYM;
+                }
+
+                // check if the number is negative and abs the number for easier parsing
+                var isNegative = amount < 0;
+                amount = Math.abs(Math.round(amount)).toString();
+
+                var formatedText = formatNumber(amount);
+
+                // add negative and symbol placeholder
+                if (isNegative) {
+                    parts.push('-');
+                }
+                parts.push(currencySymbol);
+                parts.push(formatedText);
+
+                // return the complete formatted number
+                return parts.join('');
+
+            }
+            return amount;
+        }
+
+
     });
